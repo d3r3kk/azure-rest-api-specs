522f9e6e8adc3513dd4644999fdde465d0dac852
Derek Keeler <dekeeler@microsoft.com>
2018-01-31 01:51:25 -0800

    Altered test js files to test all AppInsights files only

--- test/semantic.js
+++ test/semantic.js
@@ -7,12 +7,19 @@ var assert = require('assert'),
   oav = require('oav');
 
 describe('Azure swagger semantic validation:', function () {
-  let swaggersToProcess = utils.getFilesChangedInPR();
+  //let swaggersToProcess = utils.getFilesChangedInPR();
   // Useful when debugging a test for a particular swagger. 
   // Just update the regex. That will return an array of filtered items.
   // swaggersToProcess = swaggersToProcess.filter(function(item) {
   //   return (item.match(/.*Microsoft.Logic.*2016-06-01.*/ig) !== null);
   // });
+  let swaggersToProcess = utils.swaggers;
+  // Useful when debugging a test for a particular swagger. 
+  // Just update the regex. That will return an array of filtered items.
+  swaggersToProcess = swaggersToProcess.filter(function(item) {
+    return (item.match(/.*Microsoft.Insights.*2015-05-01.*/ig) !== null);
+  });
+
   for (const swagger of swaggersToProcess) {
     it(swagger + ' should be semantically valid.', async function () {
       try {
